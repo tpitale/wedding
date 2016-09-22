1c44be4d43d9efcae2d5292097e51fc02a224a2d
Tony Pitale <tpitale@gmail.com>
2016-09-22 12:24:33 -0500

    allow query param for code to link directly to specific rsvp

--- js/application.js
+++ js/application.js
@@ -1,3 +1,14 @@
+// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 var RSVP = RSVP || {};
 
 RSVP.getJSON = function(url, callback) {
@@ -21,7 +32,16 @@ RSVP.getJSON = function(url, callback) {
   request = null;
 }
 
-RSVP.advance_step_2 = function(e) {
+RSVP.prepare_step_1 = function() {
+  var reservation_step_1 = document.getElementById('reservation_step_1');
+  if (reservation_step_1.attachEvent) {
+    reservation_step_1.attachEvent("submit", RSVP.prepare_step_2);
+  } else {
+    reservation_step_1.addEventListener("submit", RSVP.prepare_step_2);
+  }
+}
+
+RSVP.prepare_step_2 = function(e) {
   if (e.preventDefault) e.preventDefault();
 
   var reservation_step_2 = document.getElementById('reservation_step_2');
@@ -32,12 +52,16 @@ RSVP.advance_step_2 = function(e) {
   document.getElementById('rsvp_street_number').value = street_number.value;
   document.getElementById('rsvp_postal_code').value = postal_code.value;
 
-  url = "https://rsvp.pitaleerb.com/invites/"+street_number.value+"-"+postal_code.value+".json"
-
-  RSVP.getJSON(url, RSVP.enable_step_2)
+  RSVP.advance_step_2(street_number.value, postal_code.value);
 
   // You must return false to prevent the default form behavior
   return false;
+}
+
+RSVP.advance_step_2 = function(street_number, postal_code) {
+  url = "https://rsvp.pitaleerb.com/invites/"+street_number+"-"+postal_code+".json";
+
+  RSVP.getJSON(url, RSVP.enable_step_2);
 }
 
 RSVP.enable_step_2 = function(data) {
@@ -87,11 +111,14 @@ RSVP.enable_step_2 = function(data) {
 }
 
 RSVP.rsvp = function() {
-  var reservation_step_1 = document.getElementById('reservation_step_1');
-  if (reservation_step_1.attachEvent) {
-    reservation_step_1.attachEvent("submit", RSVP.advance_step_2);
+  var code = getParameterByName("code");
+
+  if(code !== null) {
+    [street_number, postal_code] = code.split("-");
+
+    RSVP.advance_step_2(street_number, postal_code);
   } else {
-    reservation_step_1.addEventListener("submit", RSVP.advance_step_2);
+    RSVP.prepare_step_1();
   }
 }
 
